bad9c13f28d7db6a7346544ffaa2f23f830e38ae
Sam Ireland <sam.ireland.uk@gmail.com>
2017-03-09 12:20:42 +0000

    Ensure location and package name are strings

--- incipio/package.py
+++ incipio/package.py
@@ -1,4 +1,8 @@
 import os
 
 def create_package(package, location):
+    if not isinstance(package, str):
+        raise TypeError("package name must be str, not '%s'" % str(package))
+    if not isinstance(location, str):
+        raise TypeError("location must be str, not '%s'" % str(location))
     os.makedirs("%s%s%s" % (location, os.path.sep, package))
--- tests/test_package_creation.py
+++ tests/test_package_creation.py
@@ -7,3 +7,13 @@ class BasicCreationTests(IncipioTest):
     def test_can_make_package_directory(self):
         create_package("testpack", "container")
         self.assertTrue(os.path.exists("container/testpack"))
+
+
+    def test_package_must_be_str(self):
+        with self.assertRaises(TypeError):
+            create_package(100, "container")
+
+
+    def test_location_must_be_str(self):
+        with self.assertRaises(TypeError):
+            create_package("testpack", 100)
