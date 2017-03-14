2a56da3edd732fb8a03f588b0d64f6b3df18e8bc
Sam Ireland <sam.ireland.uk@gmail.com>
2017-03-14 19:03:25 +0000

    Finish full API docs

--- incipio/package.py
+++ incipio/package.py
@@ -18,7 +18,7 @@ def create_package(package, location, git=True, env=False, author=None, test=Tru
     :param bool test: If True, a test directory will be created.
     :param str license: The kind of license to use. Options are 'mit',\
     'apache', or 'gnu'."""
-    
+
     if not isinstance(package, str):
         raise TypeError("package name must be str, not '%s'" % str(package))
     if not isinstance(location, str):
@@ -42,6 +42,11 @@ def create_package(package, location, git=True, env=False, author=None, test=Tru
 
 
 def git_init(location):
+    """Initialises a git repository in the specified location. Git must be
+    installed for this to work.
+
+    :param str location: The directory to turn into a repository."""
+
     if not isinstance(location, str):
         raise TypeError("location must be str, not '%s'" % str(location))
     with open(os.devnull, 'w') as FNULL:
@@ -49,6 +54,12 @@ def git_init(location):
 
 
 def git_ignore(location, ignore=None):
+    """Adds a .gitignore file in the specified location, with some sensible
+    defaults.
+
+    :param str location: The directory to add the file to.
+    :param list ignore: A list of items to add to the .gitignore file."""
+
     if ignore is not None and not isinstance(ignore, list):
         raise TypeError("ignore argument only accepts lists")
     ignore = ignore if ignore else []
@@ -67,6 +78,11 @@ def git_ignore(location, ignore=None):
 
 
 def create_env(location, name="env"):
+    """Creates a virtual environment.
+
+    :param str location: The directory to add the env to.
+    :param str name: The name of the environment."""
+
     with open(os.devnull, 'w') as FNULL:
         subprocess.call(
          "virtualenv -p python3 %s" % os.path.sep.join([location, name]),
@@ -76,6 +92,12 @@ def create_env(location, name="env"):
 
 
 def create_python_package(location, name, author=None):
+    """Creates the actual importable Python package, with an __init__.py.
+
+    :param str location: The directory to add the package to.
+    :param str name: The name of the package.
+    :param str author: The name of the package author."""
+
     os.makedirs("%s%s%s" % (location, os.path.sep, name))
     with open(os.path.sep.join([location, name, "__init__.py"]), "w") as f:
         f.write('version = "0.1.0"\n')
@@ -83,12 +105,23 @@ def create_python_package(location, name, author=None):
 
 
 def create_test_directory(location):
+    """Creates a test directory.
+
+    :param str location: The directory to add the tests to."""
+
     os.makedirs("%s%s%s" % (location, os.path.sep, "tests"))
     with open(os.path.sep.join([location, "tests", "__init__.py"]), "w") as f:
         f.write("")
 
 
 def create_license(location, license, author):
+    """Creates a license file.
+
+    :param str location: The directory to add the file to.
+    :param str license: The kind of license to create. Valid options are 'mit',\
+    'apache', or 'gnu'.
+    :param str author: The author of the package."""
+    
     here = os.path.dirname(os.path.realpath(__file__))
     contents = None
     if license == "mit":
